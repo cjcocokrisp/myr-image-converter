9ba18db2a5ce15a2d3b035d12e2f6eb5417a6e9b
Christopher Coco <chriscoco1205@gmail.com>
2022-08-01 11:39:13 -0400

    Add comments to convert script

--- convert.py
+++ convert.py
@@ -1,25 +1,38 @@
-from PIL import Image
+try:
+    from PIL import Image
+except:
+    print("The python image library is not installed...\nInstall it using pip install pillow")
+    exit()
 
-def rgb_to_hex(rgb, mode):
+def rgb_to_hex(rgb: list[int, int, int, int] or tuple[int, int, int, int], mode: str):
     if mode == 'RGBA': return '%02x%02x%02x%02x' % rgb
     if mode == 'RGB': return '%02x%02x%02x' % rgb
 
-print('-------------------------------\nMYR Image Converter')
-file_name = input('Provide the file name: ')
-pixel_size = int(input('Please input the pixel area for the image: '))
 
+print('-------------------------------\nMYR Image Converter')
+# Get the file name and size of each pixel area 
+file_name = input('Provide the file name: ')
+pixel_size = int(input('Please input the pixel area for the image (If you do not know just use 1): '))
+
+# Open and load image then create a text file for the output
 img = Image.open(file_name)
 loaded_img = img.load()
 file = open(f'output/{file_name}.txt', 'w')
 print('Converting...')
-    
+
+# Calculate the pixels per row (IMAGE WIDTH / PIXEL SIZE)
+# The value is subtracted by 1 just to make sure there is no index out of bounds errors later in the program
 pixels_per_row = int(img.size[0] / pixel_size) - 1
 
+# Define the variables for the output
 file.write('let x = 0;\n')
 file.write('let y = 0;\n')
 file.write('let transparent = false;\n\n')
 
+# Define the loop header
 loop_header = f'while(x < {pixels_per_row + 1})' + '{\n'
+
+# Write the loops for the MYR output
 y = img.size[1] - 1
 while y > 0:
     file.write(loop_header)
@@ -38,4 +51,19 @@ while y > 0:
     file.write('x = 0;\ny++;\n\n')
     y -= pixel_size
 
-print("Conversion Successful!\n-------------------------------")
+# Loop Structure
+# while(x <= AMOUNT OF PIXELS PER ROW){
+#     if(x == 0 - PIXELS PER ROW AMOUNT){
+#         SET THE COLOR
+#     } 
+#     if(transparent == false){
+#         SET THE POSITION, THEN DRAW THE BOX AND INCREMENT
+#         setPosition(x, y, 0);
+#         box();
+#         x++;
+#     }
+# }
+
+# Tell user that the program is finished and wait for input
+print("Conversion Successful! Press enter to exit...")
+input("-------------------------------")
